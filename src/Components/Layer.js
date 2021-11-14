92835f87976521af2b7e1eb89c9d551924aaa63e
Maya Gans <jaffe.maya@gmail.com>
2021-11-14 18:45:52 -0500

    add layers

---
+++ src/Components/Layer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Layer extends React.Component {
+    render() {
+        return <div class = "layer" ></div>
+    }
+}
+
+export default Layer;
--- src/Components/Pyramid.js
+++ src/Components/Pyramid.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import Layer from './Layer'
 
 class Pyramid extends React.Component {
     render() {
-        return <div class = "pyramid" > Does this work </div>
+        return <div class = "pyramid" >
+           <Layer/>
+           <Layer/>
+           <Layer/>
+           <Layer/>
+           <Layer/>
+           <Layer/>
+        </div>
     }
 }
 
